9c8fe3a8586b1612edef194a55454d1926c09c02
Artem Mirzoian <mirzoian_041202@mail.ru>
2024-09-26 12:35:21 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
+import webbrowser
+
 import telebot
 
-MyFirstBot = telebot.TeleBot('7876492559:AAFBAv_CnHqcrdX-vGOD7aWZkJGqkpNlgus')
+bot = telebot.TeleBot('7876492559:AAFBAv_CnHqcrdX-vGOD7aWZkJGqkpNlgus')
 
 from telebot import types
 
 
-@MyFirstBot.message_handler(commands=['start'])
+@bot.message_handler()
+def lox(message):
+    while 1:
+        bot.send_message(message.chat.id, 'ты пидор!')
+
+
+@bot.message_handler(commands=['start'])
 def startBot(message):
     if message.from_user.last_name and message.from_user.first_name:
         first_mess = f"Привет, <b>{message.from_user.first_name} {message.from_user.last_name}</b>, это мой первый бот для телеграма"
     else:
         first_mess = f"Привет, <b>{message.from_user.first_name} </b>, это мой первый бот для телеграма"
     markup = types.InlineKeyboardMarkup()
-    button_yes = types.InlineKeyboardButton(text='Страница создателя', callback_data='Creator_url', url='https://t.me/asmirzoian')
-    button_no = types.InlineKeyboardButton(text='Страница педика', callback_data='Gay_url', url='https://t.me/Al1tap')
-    markup.add(button_yes)
-    markup.add(button_no)
-    MyFirstBot.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
+    button_creator = types.InlineKeyboardButton(text='Страница создателя', callback_data='Creator_url',
+                                            url='https://t.me/asmirzoian')
+    # button_gay = types.InlineKeyboardButton(text='Страница педика', callback_data='Gay_url', url='https://t.me/Al1tap')
+    markup.add(button_creator)
+    markup.add(types.InlineKeyboardButton(text='Страница педика', callback_data='Gay_url', url='https://t.me/Al1tap'))
+    bot.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
 
 
-@MyFirstBot.callback_query_handler(func=lambda call:True)
-def response(function_call):
-  if function_call.message:
-     if function_call.data == "1":
-        second_mess = ""
-        markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton("Перейти на сайт", url="https://timeweb.cloud/"))
-        MyFirstBot.send_message(function_call.message.chat.id, second_mess, reply_markup=markup)
-        MyFirstBot.answer_callback_query(function_call.id)
-    # if function_call.data == "Gay_url":
+@bot.message_handler(commands=['site', 'website'])
+def site(message):
+    bot.send_message(message.chat.id, f'<b>Открываю сайт!</b>', parse_mode='html')
+    webbrowser.open("https://youtube.com")
 
 
-MyFirstBot.infinity_polling()
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    bot.reply_to(message, 'Какой ужас')
+
+
+bot.infinity_polling()
